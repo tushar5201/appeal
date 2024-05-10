3fcb35afe43df6701450d34b1f3fde1cd1979e02
Tushar5201 <tusharlakadiya@gmail.com>
2024-05-10 20:09:31 +0530

    modified

--- src/Components/AboveHeader.jsx
+++ src/Components/AboveHeader.jsx
@@ -1,11 +1,18 @@
+import { Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function AboveHeader() {
-    return(
+    return (
         <div className="above-header-main">
             <div className="container">
-                <Link className="above-link" to="/">appeal.ecology@gmail.com</Link> &nbsp;&nbsp;|&nbsp;&nbsp;
-                <Link className="above-link" to="/">+91 93169 30971</Link>
+                <Row>
+                    <Col md={3} sm={6}>
+                        <Link className="above-link" to="/">appeal.ecology@gmail.com</Link> &nbsp;&nbsp;|&nbsp;&nbsp;
+                    </Col>
+                    <Col sm={6}>
+                        <Link className="above-link" to="/">+91 93169 30971</Link>
+                    </Col>
+                </Row>
             </div>
         </div>
     )
